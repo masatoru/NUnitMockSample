028c85f8ae99efd3deb47c469716c8bc6b470134
Satoru Fujimori <satoru.fujimori@gmail.com>
2018-03-10 14:19:14 +0900

    add SampleTest

--- NUnitMockSample/NUnitMockSample/Models/Sample.cs
+++ NUnitMockSample/NUnitMockSample/Models/Sample.cs
@@ -1,18 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NUnitMockSample.Models
 {
-    public class Sample
+    public interface ISample
+    {
+        void Method();
+        Task MethodAsync();
+    }
+
+    public class Sample : ISample
     {
         public void Method()
         {
             try
             {
                 Console.WriteLine("Hello");
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"HttpRequestException Message={ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception Message={ex.Message}");
+            }
+        }
+
+        public async Task MethodAsync()
+        {
+            try
+            {
+                await Task.Delay(1);
+                Console.WriteLine("Hello");
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"HttpRequestException Message={ex.Message}");
             }
             catch (Exception ex)
             {
--- NUnitMockSample/NUnitMockSample/SampleTest.cs
+++ NUnitMockSample/NUnitMockSample/SampleTest.cs
@@ -2,19 +2,56 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using NUnitMockSample.Models;
 
 namespace NUnitMockSample
 {
     [TestFixture]
-    public class TestClass
+    public class SampleTest
     {
         [Test]
-        public void TestMethod()
+        public void MethodMockException()
         {
-            // TODO: Add your test code here
-            Assert.Pass("Your first passing test");
+            var sample = new Mock<ISample>();
+            sample.Setup(m => m.Method())
+                .Throws(new Exception());
+
+            Assert.Throws<Exception>(() => sample.Object.Method());
         }
+
+        [Test]
+        public void MethodMockHttpRequestException()
+        {
+            var sample = new Mock<ISample>();
+            sample.Setup(m => m.Method())
+                .Throws(new HttpRequestException());
+
+            Assert.Throws<HttpRequestException>(() => sample.Object.Method());
+        }
+
+        [Test]
+        public void MethodAsyncMockException()
+        {
+            var sample = new Mock<ISample>();
+            sample.Setup(m => m.MethodAsync())
+                .Throws(new Exception());
+
+            Assert.Throws<Exception>(() => sample.Object.MethodAsync());
+        }
+
+        [Test]
+        public void MethodAsyncMockHttpRequestException()
+        {
+            var sample = new Mock<ISample>();
+            sample.Setup(m => m.MethodAsync())
+                .Throws(new HttpRequestException());
+
+            Assert.Throws<HttpRequestException>(() => sample.Object.MethodAsync());
+        }
+
     }
 }
