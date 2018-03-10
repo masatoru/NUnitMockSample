1cbacb1ac6220c1cb07815ad31c78c272b7dc3ab
Satoru Fujimori <satoru.fujimori@gmail.com>
2018-03-10 13:57:58 +0900

    add Sample class

---
+++ NUnitMockSample/NUnitMockSample/Models/Sample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitMockSample.Models
+{
+    public class Sample
+    {
+        public void Method()
+        {
+            try
+            {
+                Console.WriteLine("Hello");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception Message={ex.Message}");
+            }
+        }
+    }
+}
