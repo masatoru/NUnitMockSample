5878cd867d4f9e845c5f0956fa971b9fe3998713
Satoru Fujimori <satoru.fujimori@gmail.com>
2018-03-10 22:09:40 +0900

    add ReactiveProperty command

--- NUnitMockSample/NUnitMockSample/ViewModels/SampleViewModel.cs
+++ NUnitMockSample/NUnitMockSample/ViewModels/SampleViewModel.cs
@@ -5,16 +5,20 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using NUnitMockSample.Models;
+using Reactive.Bindings;
 
 namespace NUnitMockSample.ViewModels
 {
     public class SampleViewModel
     {
         private ISample Sample { get; set; }
+        public ReactiveCommand SampleMethodCommand { get;}
 
         public SampleViewModel(ISample sample)
         {
             Sample = sample;
+            SampleMethodCommand = new ReactiveCommand();
+            SampleMethodCommand.Subscribe(async () => await ViewModelMethod());
         }
 
         public async Task ViewModelMethod()
